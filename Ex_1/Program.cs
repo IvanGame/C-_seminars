5dbea07df6f2daff77a76b4fd4431a959e6e89a2
IvanGame <ivan5456007@gmail.com>
2022-12-20 16:49:12 +0700

    Ex_1 compleated

---
+++ Ex_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1
+using System;
+
+Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"Число {num2} больше числа {num1}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
