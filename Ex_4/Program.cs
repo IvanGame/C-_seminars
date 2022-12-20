d6a1963af4443091927b00a99ef63c25b0f56e42
IvanGame <ivan5456007@gmail.com>
2022-12-20 17:14:57 +0700

    Ex_4 compleated

---
+++ Ex_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Запрашиваем у пользователя число N
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine()!);
+
+// Создаём счётчик
+int count = 1;
+
+// Перебираем и выводим чётные числа
+Console.Write("Чётные числа от 1 до " + N + ": ");
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        System.Console.Write($"{count} ");
+    }
+    count++;
+}
