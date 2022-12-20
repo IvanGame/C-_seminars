43135c552ce1d98908154e8e9bf4dea05c7ec5ba
IvanGame <ivan5456007@gmail.com>
2022-12-20 16:56:39 +0700

    Ex_2 compleated

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -1,12 +1,15 @@
 ﻿// Задача 1
 using System;
 
+// Запрашиваем  у пользователя первое число
 Console.WriteLine("Введите первое число:");
 int num1 = int.Parse(Console.ReadLine());
 
+// Запрашиваем  у пользователя второе число
 Console.WriteLine("Введите второе число:");
 int num2 = int.Parse(Console.ReadLine());
 
+// Сравниваем числа
 if (num1 > num2)
 {
     Console.WriteLine($"Число {num1} больше числа {num2}");
--- Ex_2/Program.cs
+++ Ex_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Запрашиваем у пользователя три числа
+Console.WriteLine("Введите три числа:");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+// Находим максимальное число
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+
+// Выводим результат
+Console.WriteLine("Максимальное число: " + max);
