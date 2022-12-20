f8a16fa4dae4e4511e490a68aaa9a23ba6b9dab3
IvanGame <ivan5456007@gmail.com>
2022-12-20 17:05:35 +0700

    Ex_3 compleated

---
+++ Ex_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Запрашиваем у пользователя число
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+// Проверяем, является ли число чётным
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
