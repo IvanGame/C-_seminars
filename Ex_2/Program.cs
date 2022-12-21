597b36de37a408753d8413d2b8079faf617ac78f
IvanGame <ivan5456007@gmail.com>
2022-12-21 21:59:42 +0700

    Ex_2 compleated

--- Ex_2/Program.cs
+++ Ex_2/Program.cs
@@ -1,19 +1,29 @@
-﻿// Запрашиваем у пользователя три числа
-Console.WriteLine("Введите три числа:");
-int num1 = int.Parse(Console.ReadLine());
-int num2 = int.Parse(Console.ReadLine());
-int num3 = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
 
-// Находим максимальное число
-int max = num1;
-if (num2 > max)
+if (number < 100)
 {
-    max = num2;
+    Console.WriteLine("У введенного числа нет третьей цифры!");
 }
-if (num3 > max)
+else if (number > 100000)
 {
-    max = num3;
+    Console.WriteLine("Число больше положенного диапазона. Ввкдите число до 100000");
 }
-
-// Выводим результат
-Console.WriteLine("Максимальное число: " + max);
+else
+{
+    if(number > 99 && number < 1000)
+    {
+        int thirdDigit = number % 10;
+        Console.WriteLine($"Третья цифра введенного числа: {thirdDigit}");
+    }
+    else if (number > 999 && number < 10000)
+    {
+        int thirdDigit = number / 10 % 10;
+        Console.WriteLine($"Третья цифра введенного числа: {thirdDigit}");
+    }
+    else
+    {
+        int thirdDigit = number / 100 % 10;
+        Console.WriteLine($"Третья цифра введенного числа: {thirdDigit}");
+    }
+}
