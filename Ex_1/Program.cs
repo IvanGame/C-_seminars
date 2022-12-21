2586e6bc347fa55cc9ba74c029297010c1f48229
IvanGame <ivan5456007@gmail.com>
2022-12-21 21:20:08 +0700

    Ex_1 compleated

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -1,24 +1,12 @@
-﻿// Задача 1
-using System;
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
 
-// Запрашиваем  у пользователя первое число
-Console.WriteLine("Введите первое число:");
-int num1 = int.Parse(Console.ReadLine());
-
-// Запрашиваем  у пользователя второе число
-Console.WriteLine("Введите второе число:");
-int num2 = int.Parse(Console.ReadLine());
-
-// Сравниваем числа
-if (num1 > num2)
+if (number < 100 || number > 999)
 {
-    Console.WriteLine($"Число {num1} больше числа {num2}");
-}
-else if (num1 < num2)
-{
-    Console.WriteLine($"Число {num2} больше числа {num1}");
+    Console.WriteLine("Вы ввели не трехзначное число!");
 }
 else
 {
-    Console.WriteLine("Числа равны");
+    int secondDigit = (number / 10) % 10;
+    Console.WriteLine($"Вторая цифра введенного числа: {secondDigit}");
 }
