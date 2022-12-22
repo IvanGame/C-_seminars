65a3ad33c3101bbaa814a23ea2b3e362bc9431bc
IvanGame <ivan5456007@gmail.com>
2022-12-22 22:37:31 +0700

    Ex_1 compleated

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -1,12 +1,47 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
-int number = int.Parse(Console.ReadLine());
-
-if (number < 100 || number > 999)
+﻿/* Математический способ 
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+if (number > 9999 && number < 100000)
 {
-    Console.WriteLine("Вы ввели не трехзначное число!");
+    int a = number / 10000;
+    int b = (number / 1000) % 10;
+    int c = (number / 100) % 10;
+    int d = (number / 10) % 10;
+    int e = number % 10;
+
+    if (a == e && b == d)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+
 }
 else
 {
-    int secondDigit = (number / 10) % 10;
-    Console.WriteLine($"Вторая цифра введенного числа: {secondDigit}");
+    Console.WriteLine("Вы ввели не пятизначное число!");
+} */
+
+// Альтернативный способ
+
+Console.WriteLine("Введите пятизначное число:");
+
+string number = Console.ReadLine()!;
+
+if (number.Length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не пятизначное число!");
 }
